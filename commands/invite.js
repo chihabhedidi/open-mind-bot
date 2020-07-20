f4b47c1ac6753f9d161176903f8c99dacea120a7
chihabhedidi <68205628+chihabhedidi@users.noreply.github.com>
2020-07-20 23:46:34 +0100

    first

---
+++ commands/invite.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js")
+const botconfig = require("../botsettings.json");
+const { MessageEmbed } = require('discord.js')
+module.exports.run = async (bot, message, args) => {
+
+    const embed = new MessageEmbed()
+    
+    .addFields(
+        
+        {
+        name:"Invite the bot to your server",
+        value: "[**Invite**](https://discord.com/api/oauth2/authorize?client_id=731315754175037480&permissions=8&scope=bot)"
+        },
+    )
+    .setColor('#fc0b03')
+    
+
+  
+await message.channel.send(embed)
+
+}
+module.exports.config = {
+    name: "invite",
+    description: "invite the bot to your server",
+    usage: "*invite",
+    accessableby: "Members",
+    aliases: []
+}
+
