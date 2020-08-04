4adbca660f2a4121df97a71a889c04418108118a
chihabhedidi <68205628+chihabhedidi@users.noreply.github.com>
2020-08-04 20:16:00 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => { 
     console.log('your bot is on!');
- bot.user.setActivity('thelp', { type: 'WATCHING' });
+ bot.user.setActivity('m!help', { type: 'WATCHING' });
 }
