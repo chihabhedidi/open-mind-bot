3162e8a2c4f3d2e6bd9ff1072c64094188069c8a
chihabhedidi <68205628+chihabhedidi@users.noreply.github.com>
2020-09-05 16:43:09 +0100

    scrennshot command

--- commands/help.js
+++ commands/help.js
@@ -253,6 +253,11 @@ module.exports.run = async (bot, message, args) => {
                 inline: true
             },
             {
+                name: ":computer:** - \`screenshot\`**",
+                value: `**Screenshot** To display a specific website`,
+                inline: true
+            },
+            {
                 name: ":regional_indicator_x::o2:** - \`xo\`**",
                 value: `**Xo** To start xo game`,
                 inline: true
