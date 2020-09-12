9f907dac86b68b05f44f31e07b8b0ddac3bb3367
chihabhedidi <68205628+chihabhedidi@users.noreply.github.com>
2020-09-12 13:29:10 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -7,6 +7,10 @@ module.exports.run = async (bot, message, args) => {
     message.delete().catch(err=>console.log(err));
     if(message1.toLowerCase().includes("token")){
         return message.channel.send(`<:noice:749960391869857822>`);
+    }if(message1.toLowerCase().includes("@everyone")){
+        return message.channel.send(`am not allowed to mention everyone`);
+    }if(message1.toLowerCase().includes("@here")){
+        return message.channel.send(`am not allowed to mention everyone`);
     }
     message.channel.send(`${message1}`);
 }
