4cee7e727556c1c556f32f8c424a519f1bf5f68c
chihabhedidi <68205628+chihabhedidi@users.noreply.github.com>
2020-09-12 12:01:33 +0200

    Update unlock.js

--- commands/unlock.js
+++ commands/unlock.js
@@ -8,10 +8,13 @@ module.exports.run = async (bot, message, args) => {
   return message.channel.send("You don't have permission to use that command.");
   
         
-        
+        try{
       message.channel.updateOverwrite(message.channel.guild.roles.everyone, {SEND_MESSAGES: true });
       return message.channel.send("Successfully unlocked this channel");
+        }catch (err) {
+    return message.reply(`\`${err.message}.!\``);
 
+}
 }
 
 module.exports.config = {
