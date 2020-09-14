6a211eed9ee8011b64c98e91c01b60542202d84e
chihabhedidi <68205628+chihabhedidi@users.noreply.github.com>
2020-09-14 22:39:36 +0200

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -5,19 +5,25 @@ const botconfig = require("../botsettings.json");
 
 
 module.exports.run = async (bot, message, args) => {
-    if(message.author.bot) return;
-    if(!message.member.hasPermission('MANAGE_ROLE'))
+    if(!message.member.hasPermission('ADMINISTRATOR'))
     return message.channel.send("You don't have permission to use that command.");
     const user = message.mentions.users.first();    
     if(user) {
         const member = message.guild.member(user);
         if(member){ 
     let mutedRole = message.guild.roles.cache.find(x => x.name === "Muted")
-        try{
-        if(mutedRole) {
+    if(!mutedRole){return message.channel.send("the server doesnt have a  Mute role please use  command to auto create it")}
+
+    try{
+        if(member.roles.cache.has(mutedRole.id)){
             member.roles.remove(mutedRole);
-            return message.channel.send("User was Successfully Unmuted.");
-        }}catch (err) {
+            return message.channel.send(`${member}was Successfully Unmuted `);
+           }else
+     {
+        
+        return message.channel.send("user is not  muted!");
+    }
+}catch (err) {
     return message.reply(`\`${err.message}.!\``);
 
 }
@@ -26,8 +32,9 @@ module.exports.run = async (bot, message, args) => {
 }
 
 }else{return message.channel.send("Please please mention member")}
-
+    
 }
+
 
 module.exports.config = {
     name: "unmute",
