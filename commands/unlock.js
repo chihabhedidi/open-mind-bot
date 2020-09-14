93af0c2fa0ba50309a78e429b76228992e707fe7
chihabhedidi <68205628+chihabhedidi@users.noreply.github.com>
2020-09-14 22:38:28 +0200

    Update unlock.js

--- commands/unlock.js
+++ commands/unlock.js
@@ -3,18 +3,25 @@ const botconfig = require("../botsettings.json");
 
 
 module.exports.run = async (bot, message, args) => {
-    if(message.author.bot) return;
-  if(!message.member.hasPermission('MANAGE_ROLES'))
+    
+  if(!message.member.hasPermission('ADMINISTRATOR'))
   return message.channel.send("You don't have permission to use that command.");
   
+     
+     try{ 
+        if(message.channel.name.startsWith("🔒")) {
+            message.channel.setName(message.channel.name.substring(1))
+           message.channel.updateOverwrite(message.channel.guild.roles.everyone, {SEND_MESSAGES: true });
+      return message.channel.send("Successfully unlocked this channel");}
         
-        try{
-      message.channel.updateOverwrite(message.channel.guild.roles.everyone, {SEND_MESSAGES: true });
-      return message.channel.send("Successfully unlocked this channel");
-        }catch (err) {
-    return message.reply(`\`${err.message}.!\``);
+        else{
+            return message.channel.send("this channel is not locked")
+        }
+     
+    } catch (err) {
+        return message.reply(`\`${err.message}.!\``);
 
-}
+    }
 }
 
 module.exports.config = {
