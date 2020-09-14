a0e56a87ad2dda1f645b5745ff9ac5d877eb126b
chihabhedidi <68205628+chihabhedidi@users.noreply.github.com>
2020-09-14 22:32:02 +0200

    Update lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -3,19 +3,24 @@ const botconfig = require("../botsettings.json");
 
 
 module.exports.run = async (bot, message, args) => {
-    if(message.author.bot) return;
-  if(!message.member.hasPermission('MANAGE_ROLES'))
+    
+  if(!message.member.hasPermission('ADMINISTRATOR'))
   return message.channel.send("You don't have permission to use that command.");
   
         
-        try{
+       try{ 
+        if(message.channel.name.startsWith("🔒")) {return message.channel.send("this channel is already locked")}
+         message.channel.setName(`🔒${message.channel.name}`)
       message.channel.updateOverwrite(message.channel.guild.roles.everyone, {SEND_MESSAGES: false });
+     
+      
       return message.channel.send("Successfully locked this channel");
-        }catch (err) {
-    return message.reply(`\`${err.message}.!\``);
+       }catch (err) {
+        return message.reply(`\`${err.message}.!\``);
+    
+    }
+}
 
-}
-}
 
 module.exports.config = {
     name: "lock",
