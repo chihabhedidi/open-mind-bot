66fefe62b0198884729bab67fd83c4c6959a03e1
chihabhedidi <68205628+chihabhedidi@users.noreply.github.com>
2020-09-30 21:33:47 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -4,6 +4,7 @@ const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
     const message1=args.join(" ");
+if(!message1)return message.channel.send(`Usage:say (message)`)
     message.delete().catch(err=>console.log(err));
     if(message1.toLowerCase().includes("token")){
         return message.channel.send(`<:noice:749960391869857822>`);
