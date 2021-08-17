6b692e0b9190418a8fde3b401309fe92569f91b1
chihabhedidi <68205628+chihabhedidi@users.noreply.github.com>
2021-08-17 22:18:45 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -8,7 +8,7 @@ const botconfig = require("../botsettings.json");
 module.exports.run = async (bot, message, args) => {
   if(message.author.bot) return;
   message.channel.send('Pong...').then((msg) => {
-    msg.edit(`\`\`\`javascript\nTime taken: ${msg.createdTimestamp - message.createdTimestamp} ms.\nPing: ${Math.round(bot.ws.ping)} ms.\`\`\``);//حقوق دايموند كودز
+    msg.edit(`\`\`\`javascript\nTime taken: ${msg.createdTimestamp - message.createdTimestamp} ms.\nPing: ${Math.round(bot.ws.ping)} ms.\`\`\``);ايموند كودز
 })
 }
 
